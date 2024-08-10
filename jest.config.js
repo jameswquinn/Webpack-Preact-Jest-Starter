0be40e3955312611ea2092d83abd20f4013a8786
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-10 21:26:27 +0100

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
