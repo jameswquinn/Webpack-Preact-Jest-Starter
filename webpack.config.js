8d642935cc610f741f3d9f45f6d1e16061ae9eae
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-10 22:37:59 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -174,9 +174,15 @@ module.exports = (env, argv) => {
       static: {
         directory: path.join(__dirname, 'public'),
       },
-      hot: true,
-      historyApiFallback: true,
+      compress: true,
       port: 3000,
+      hot: true,
+      allowedHosts: 'all',
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      },
     },
     resolve: {
       alias: {
