c8d35424433468eb25e94f7ba93fd4536b7fefb9
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 07:40:59 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { pragma: 'h', pragmaFrag: 'Fragment' }],
-  ],
-};
+{
+  "presets": [
+    "@babel/preset-env",
+    ["@babel/preset-react", { "pragma": "h", "pragmaFrag": "Fragment" }]
+  ]
+}
