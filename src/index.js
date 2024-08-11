a6653ea41dd0a6ed6b44a0ed1f1edfe357890eda
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 10:34:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import { h, render } from 'preact';
 import App from './App';
 
-// Remove this line if you don't have a styles.css file
-// import './styles.css';
-
-render(<App />, document.body);
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
-  });
-}
+render(<App />, document.getElementById('app'));
