c3c11b9449b32d18089828db0587654554a12398
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 07:51:14 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,101 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { processImage } = require('./src/utils/imageHelper');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', ['@babel/preset-react', { pragma: 'h', pragmaFrag: 'Fragment' }]],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', ['@babel/preset-react', { pragma: 'h', pragmaFrag: 'Fragment' }]],
+            },
           },
         },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'postcss-loader'
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      isProduction && new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'public',
+            to: 'assets',
+            globOptions: {
+              ignore: ['**/*.png'],
+            },
+          },
+          {
+            from: 'public',
+            to: 'assets',
+            globOptions: {
+              ignore: ['**/*.!(png)'],
+            },
+            transform: async (content, path) => {
+              if (path.endsWith('.png')) {
+                const result = await processImage(content);
+                return result.webp; // Return WebP version as primary
+              }
+              return content;
+            },
+          },
+        ],
+      }),
+    ].filter(Boolean),
+    optimization: {
+      minimizer: [
+        new TerserPlugin(),
+        new CssMinimizerPlugin(),
+      ],
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
       },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
+      compress: true,
+      port: 3000,
+      hot: true,
+      allowedHosts: 'all',
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      },
+    },
+    resolve: {
+      alias: {
+        'react': 'preact/compat',
+        'react-dom': 'preact/compat',
+      },
+    },
+  };
 };
