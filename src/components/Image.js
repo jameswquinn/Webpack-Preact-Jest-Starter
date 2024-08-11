a00343bf060e11af7259babd332b90ef90a0373e
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 07:48:38 +0100

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,17 +2,11 @@ import { h } from 'preact';
 
 function Image({ src, alt, className }) {
   return (
-    <picture>
-      <source type="image/webp" srcSet={src.srcSet} sizes="(max-width: 600px) 300px, (max-width: 1200px) 600px, 1200px" />
-      <img 
-        src={src.fallbackSrc} 
-        srcSet={src.fallbackSrcSet}
-        sizes="(max-width: 600px) 300px, (max-width: 1200px) 600px, 1200px"
-        alt={alt}
-        className={className}
-        style={src.isTransparent ? { backgroundColor: 'transparent' } : {}}
-      />
-    </picture>
+    <img 
+      src={src}
+      alt={alt}
+      className={className}
+    />
   );
 }
 
