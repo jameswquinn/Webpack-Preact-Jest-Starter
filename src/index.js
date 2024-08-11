0e3739c2f6f6db677da7aed207404f5db961c8d9
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 08:52:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { h, render } from 'preact';
-
-function App() {
-  return <div>Hello, World!</div>;
-}
+import App from './App';
+import './styles.css';
 
 render(<App />, document.body);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
