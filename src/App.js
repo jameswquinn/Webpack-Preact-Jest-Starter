be37965fd9208b03a5599e69acb3a8326aa9f7c5
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 09:26:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { h } from 'preact';
 import Image from './components/Image';
-import exampleImage from '../assets/example.png';
+import exampleImage from '../public/example.png';
 
-function App() {
+function MyComponent() {
   return (
     <div>
-      <h1>Hello, Preact!</h1>
+      <h1>My Component</h1>
       <Image src={exampleImage} alt="Example" />
     </div>
   );
 }
 
-export default App;
+export default MyComponent;
