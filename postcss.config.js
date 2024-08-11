240e07bb46803505d7bcb262470b69e62e73f0a9
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 07:41:51 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   plugins: [
-    'autoprefixer',
-    'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-      },
-    ],
+    require('autoprefixer'),
   ],
 };
