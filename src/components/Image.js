7361005bd248c5bcda062abdd65f0472d9ee9c42
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-11 09:25:27 +0100

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,8 +3,9 @@ import { h } from 'preact';
 function Image({ src, alt, className }) {
   return (
     <img 
-      srcSet={`${src.srcSet}`}
-      src={src.fallbackSrc}
+      srcSet={src.srcSet}
+      src={src.src}
+      sizes="(max-width: 300px) 300px, (max-width: 600px) 600px, (max-width: 1200px) 1200px, 2000px"
       alt={alt}
       className={className}
     />
