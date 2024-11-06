abc4b84a6adef7e3396f6862f4aad9e152828c11
jameswquinn <jameswquinn@users.noreply.github.com>
2024-11-06 18:49:40 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import { h, render } from 'preact';
 import App from './App';
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('root'));
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
