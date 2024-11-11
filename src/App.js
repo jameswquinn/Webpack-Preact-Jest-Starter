32dae8c99e1495a390d5017025687b0d06ca9ea1
jameswquinn <jameswquinn@users.noreply.github.com>
2024-11-11 00:18:21 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { h } from 'preact';
 const App = () => (
   <div>
     <h1>Hello, World!</h1>
-<img src="/assets/logo.png" alt="Description of image" />
+<img src="./assets/logo.png" alt="Description of image" />
   </div>
 );
 
